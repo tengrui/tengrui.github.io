36e27be90393014ddca866282042561b8f6c9f5d
Rui Teng <tengrui@hotmail.com>
2023-01-17 19:29:56 +0800

    update
    Signed-off-by: Rui Teng <tengrui@hotmail.com>

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230117.001037';
+const cacheName = 'chirpy-20230117.192927';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
